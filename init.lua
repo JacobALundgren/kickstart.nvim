84c275c9c5a993c28f277de58d17db25caf13bb0
Jacob Lundgren <jacob.a.lundgren@gmail.com>
2023-10-12 08:19:24 +0100

    Missing indentline config

--- init.lua
+++ init.lua
@@ -602,5 +602,16 @@ cmp.setup {
   },
 }
 
+require('ibl').setup(
+  {
+    indent = {
+      char = '┊',
+    },
+    whitespace = {
+      remove_blankline_trail = true,
+    },
+  }
+)
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
